825921d535753bfd5ef9ee97f189b7c062ca3beb
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-12-12 15:54:27 -0200

    :construction: wip(learn): methods

--- ES6-study/startupModule/src/app.js
+++ ES6-study/startupModule/src/app.js
@@ -1,23 +1,33 @@
 class Drone {
     constructor(id, name) {
-        // instance variables/properties with this keyword
         this.id = id;
         this.name = name;
-        // class variables/properties
+    }
+
+    static getCompany() {
+        // create a static method for Drone class
+        console.log('in getCompany');
+
+        console.log(this.id); //  can't access instance properties
+        
+        
+    }
+
+    fly() {
+        // create a method for Drone instances (is like functions)
+        console.log('Drone ' + this.id + ' is flying to the moon'); // when working with instance of class, alway have to use 'this' keyword
+        
     }
 }
-Drone.maxHeight = 2000; // set a static/class property
 
-// static properties can not be accessed by instances like drone2.maxHeight
 
 let drone = new Drone('A123', 'Flyer');
-
-// now the instance properties are accessible
-console.log('drone: ' + drone['id'] + ' ' + drone.name); // two possible notation to access properties 
-
 let drone2 = new Drone('B456', 'Twirl');
 
-console.log(drone.id + ' ' + drone2.id); // each Drone instance has it own properties set
+drone.fly(); // call a method
+drone2.fly();
 
+Drone.getCompany(); // call a static method
 
+drone.getCompany(); // is not accessible throug instances: trows ans error
 
