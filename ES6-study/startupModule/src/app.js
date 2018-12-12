69b9db0a62b91d0b9eec09ed897a1baa96f81032
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-12-12 15:16:24 -0200

    :construction: wip(learn): finish instance properties

--- ES6-study/startupModule/src/app.js
+++ ES6-study/startupModule/src/app.js
@@ -1,10 +1,20 @@
 class Drone {
     constructor(id, name) {
-        console.log('in Drone constructor'); // log the message when instantiated
-        console.log('id: ' + id); // log the attribute id passed when instantiated
-        console.log(Date() + ' - ' + id + ' ' + name );   
+        // instance variables/properties with this keyword
+        this.id = id;
+        this.name = name;
+        // class variables/properties
     }
 }
 
 let drone = new Drone('A123', 'Flyer');
+
+// now the instance properties are accessible
+console.log('drone: ' + drone['id'] + ' ' + drone.name); // two possible notation to access properties 
+
+let drone2 = new Drone('B456', 'Twirl');
+
+console.log(drone.id + ' ' + drone2.id); // each Drone instance has it own properties set
+
+
 
