9471451d92084711acada98fcae0594284f35aac
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-12-13 16:05:16 -0200

    :construction: wip(learn): inheriting methods

--- ES6-study/startupModule/src/app.js
+++ ES6-study/startupModule/src/app.js
@@ -1,20 +1,36 @@
 class Vehicle {
-    constructor() {
-        this.gpsEnabled = true; // instance property in abstract class, not static
+    constructor(licenceNum) {
+        this.licenceNum = licenceNum;
     }
-}
-
-class Drone extends Vehicle {
-    
+    start() {
+        console.log('starting Vehicle ' + this.licenceNum); // main start method
+    }
+    static getCompanyName() {
+        console.log('My Company');
+        
+    }
 }
 
 class Car extends Vehicle {
-    constructor() {
-        super();
-        this.gpsEnabled = false; // overrides Vehicle property
+    constructor(licenceNum) {
+        super(licenceNum);
+    }
+    start() {
+        console.log('starting Car ' + this.licenceNum); // overrides Vehicle.start()
+        super.start(); // will call Vehicle.start() after (can be placed anywhere) the inherit overrided method        
+    }
+    static getCompanyName() { // if call Car.getCompanyName, will return My Other Company, because this static method overrides Vehicle method
+        super.getCompanyName(); // calls Vehicle.getCompanyName before Car.getCompanyName
+        console.log('My Other Company');
+        
     }
 }
 
-let c = new Car();
+let c = new Car('A123');
 
-console.log(c.gpsEnabled);
+c.start();
+
+
+Car.getCompanyName(); // because Car is a Vehicle, will return what expected
+
+c.getCompanyName(); // cant call, only belongs Vehicle and Car, not instances of classes
