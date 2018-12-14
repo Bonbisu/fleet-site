14b5732af69a4716dfbfe52a3fe989ee10cde708
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-12-14 15:42:14 -0200

    :construction: wip(learn): checking valid data

--- ES6-study/startupModule/src/services/fleet-data-service.js
+++ ES6-study/startupModule/src/services/fleet-data-service.js
@@ -14,13 +14,29 @@ export class FleetDataService {
         for (let data of fleet) {
             switch (data.type) {
                 case 'car':
-                    let car = this.loadCar(data);
-                    this.cars.push(car);
+                    if (this.validateCarData(data) ) { // execute if have no errors
+                        let car = this.loadCar(data);
+                        if (car) {
+                            this.cars.push(car);
+                        }
+                    } else {
+                        let e = new DataError('invalid car data', data);
+                        this.errors.push(e);
+                    }
                     break;
+
                 case 'drone':
-                    let drone = this.loadDrone(data);
-                    this.drones.push(drone);
+                    if (this.validateDroneData(data) ) {
+                        let drone = this.loadDrone(data);
+                        if (drone) {
+                            this.drones.push(drone);
+                        } 
+                    } else {
+                            let e = new DataError('invalid drone data', data);
+                            this.errors.push(e);
+                    }
                     break;
+
                 default:
                     let e = new DataError('Invalid vehicle type', data);
                     this.errors.push(e);
@@ -42,9 +58,50 @@ export class FleetDataService {
     }
 
     loadDrone(drone) {
-        let d = new Drone(drone.license, drone.model, drone.latLong);
-        d.airTimeHour = drone.airTimeHour;
-        d.base = drone.base;
-        return d;
+        try {
+            let d = new Drone(drone.license, drone.model, drone.latLong);
+            d.airTimeHour = drone.airTimeHour;
+            d.base = drone.base;
+            return d;
+        } catch(e) {
+            this.errors.push(new DataError('error loading drone', drone) );
+        }
+        return null;
+    }
+
+    validateCarData(car) {
+        let requiredProps = 'license model latLong miles make'.split(' ');
+        let hasErrors = false;
+
+        for (let field of requiredProps) {
+            if ( ! car[field] ) {
+                this.errors.push(new DataError(`invalid field ${field}`, car) ); // ES6 backtick syntax allows to use iterated variable
+                hasErrors = true;
+            }
+        }
+
+        if (Number.isNaN(Number.parseFloat(car.miles) ) ) {
+            this.errors.push(new DataError('invalid mileage', car) );
+            hasErrors = true;
+        }
+        return !hasErrors; // send true if no errors
+    }
+
+    validateDroneData(drone) {
+        let requiredProps = 'license model latLong airTimeHours base'.split(' ');
+        let hasErrors = false;
+        
+        for (let field of requiredProps) {
+            if ( ! drone[field] ) {
+                this.errors.push(new DataError(`invalid field ${field}`, drone) );
+                hasErrors = true;
+            }
+        }
+
+        if (Number.isNaN(Number.parseFloat(drone.airTimeHours) ) ) {
+            this.errors.push(new DataError('invalid air time', drone) );
+            hasErrors = true;
+        }
+        return !hasErrors;
     }
 }
