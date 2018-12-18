9ff4d0753318353f8e6be5766c13c7b74b249e45
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-12-18 18:27:56 -0200

    :construction: wip(learn): put created elements on app.js

--- ES6-study/startupModule/src/app.js
+++ ES6-study/startupModule/src/app.js
@@ -1,16 +1,38 @@
+import $ from "jquery";
 import { Car } from "./classes/car.js";
 import { Drone } from "./classes/drone.js";
 import { fleet } from "./fleet-data.js";
 import { FleetDataService } from "./services/fleet-data-service.js";
+import { Button } from "./ui/button.js";
+import { Image } from "./ui/image.js";
+import { TitleBar } from "./ui/title-bar.js";
+import { DataTable } from "./ui/data-table.js";
+import { GoogleMap } from "./ui/google-map.js";
 
+let tb = new TitleBar('Our Application');
+tb.addLink('Home', '');
+tb.addLink('Cars', '');
+tb.addLink('Drones', '');
+tb.addLink('Map', '');
+tb.appendToElement($('body'));
+
+let i = new Image('../images/drone.jpg');
+i.appendToElement($('body'));
+
+
+let headers = 'License Make Model Miles'.split(' ');
 let dataService = new FleetDataService();
 dataService.loadData(fleet);
 
-// let car = dataService.getCarByLicense('AT9900'); 
 
-// let cars = dataService.getCarsSortedByLicense();
+let dt = new DataTable(headers, dataService.cars);
+dt.appendToElement($('body'));
 
-let cars = dataService.filterCarsByMake('U');
+let b = new Button('Click Me');
+b.appendToElement($('body'));
 
-for (let car of cars)
-    console.log(car);  
+let centerOfMap = {lat: 40.783661, lng: -73.965883};
+
+let map = new GoogleMap(centerOfMap, dataService.cars);
+map.appendToElement($('body'));
+
