17be696fe3561038d1b13d0df85488dd749955a4
bonbisu <fernando.costa30@fatec.sp.gov.br>
2018-12-18 18:23:50 -0200

    :construction: wip(learn): create a base element ui class

---
+++ ES6-study/startupModule/src/ui/base-element.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+
+export class BaseElement {
+
+    constructor() {
+        this.element = null; // jQuery object
+    }
+
+    appendToElement(el) {
+        this.createElement();
+        el.append(this.element);
+        this.enableJS();
+    }
+
+    createElement() {
+        let s = this.getElementString();
+        this.element = $(s);
+    }
+
+    getElementString() {
+        throw 'Please override getElementString() in BaseElement';
+    }
+
+    enableJS() {
+        componentHandler.upgradeElement(this.element[0]); // mdl funtion that allows JS (/wo this, it don't recognize that we create an Material elemente)
+    }
+}
